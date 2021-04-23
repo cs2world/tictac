22590f31c2f653b7bd38d2c1781f9cf1c02280e4
P.Y. Cheng <cs2world@gmail.com>
2021-04-22 23:39:51 -0700

    Solve the logic error in move() and restart(), what about close?

--- READMD.md
+++ READMD.md
@@ -3,5 +3,13 @@
 This example is to demonstrate a basic EOSIO smart contract, including
 
 - Define contract actions
+    - create
+    - close
+    - move
 - Define a table
+    - games_table, a multi_index table on the eosio-blockchain, how to remove the existing table?
+    -
 - Perform read/write/remove operations on the table
+    - emplace
+    - modify 
+    - the lamda function does the key work for inserting and modifying.
--- src/tictac3.cpp
+++ src/tictac3.cpp
@@ -32,8 +32,8 @@ uint8_t tictac3::get_winner(uint32_t _board){
   uint32_t win_pattern;
   for(int i=0; i<8; i++) {
     win_pattern = _board & CHECK_WIN_PATTERN[ i ];
-    if(win_pattern == HOST_WIN_PATTERN[ i ])    return 1; // winner is host.
-    if(win_pattern == SET_CELL_CHALLENGER[ i ]) return 2; // chanllenger wins.
+    if(win_pattern ==       HOST_WIN_PATTERN[ i ]) return 1; // winner is host.
+    if(win_pattern == CHALLENGER_WIN_PATTERN[ i ]) return 2; // chanllenger wins.
   }
   return 0; // no winner
 }
@@ -80,6 +80,47 @@ ACTION tictac3::close( const name challenger, const name host ) {
   check( false, "No game record found for you!");
 }
 
+/**
+** find the player in host/primary index or challenger/2nd index
+** then update the game record to the initial state 
+*/
+ACTION tictac3::restart(name player){
+  check( has_auth( player), "You shold has the permission to play.");
+  
+  games_table _games(get_self(), get_self().value);
+  auto primary_itr = _games.find(player.value);
+
+  auto idx = _games.get_index<name("bychallenger")>();
+  auto index_itr = idx.find(player.value);
+  
+  if( (primary_itr != _games.end()) && (index_itr != idx.end()) ){
+    check( false, "You have more than one games....");
+  }
+
+  if( (primary_itr != _games.end()) && (index_itr == idx.end()) ){ // is a host
+      _games.modify(primary_itr, get_self(), [&] (auto& this_game){
+      this_game.board = 0; // clear the board
+      this_game.winner = 0;
+      this_game.turns = 0;
+      return;
+    });
+  }
+  
+  if( (primary_itr == _games.end()) && (index_itr != idx.end()) ){ // is a challenger
+    idx.modify(index_itr, get_self(), [&](auto& this_game){
+      this_game.board = 0; // clear the board
+      this_game.winner = 0;
+      this_game.turns = 0;
+      return;
+    });
+  }
+
+  if( (primary_itr == _games.end()) && (index_itr == idx.end()) ){
+    check( false, "You have no game to restart....");
+  }
+
+}
+
 ACTION tictac3::move( name player, uint16_t x, uint16_t y){
   check( has_auth( player), "You shold has the permission to play.");
   // x, y has three valid values [0,1,2], any value>2 will be set=2.
@@ -88,43 +129,48 @@ ACTION tictac3::move( name player, uint16_t x, uint16_t y){
   uint16_t position = x + 3 * y;
   // find the game record first in the games table
   games_table _games(get_self(), get_self().value);
+  auto primary_itr = _games.find(player.value);
+
+  auto idx = _games.get_index<name("bychallenger")>();
+  auto index_itr = idx.find(player.value);
   
-  auto game_itr = _games.find(player.value);
-  if (game_itr != _games.end() ) {// player is the host
-    check(game_itr->winner == 0, "Game over: Winner=1: Host; 2: Challenger; 3: Draw");
-    uint8_t _turns = game_itr->turns; 
+  if( (primary_itr != _games.end()) && (index_itr != idx.end()) ){
+    check( false, "You have more than one games. one game at a time, please.");
+  }
+
+  if( (primary_itr == _games.end()) && (index_itr == idx.end()) ){
+    check( false, "You have no game to for the move....");
+  }
+
+  if( (primary_itr != _games.end()) && (index_itr == idx.end()) ){ // is a host
+    check(primary_itr->winner == 0, "Game over: Winner=1: Host; 2: Challenger; 3: Draw");
+    uint8_t _turns = primary_itr->turns; 
+    
     check( _turns % 2 == 1, "This is the Challenger's turn!" );
-    check( game_itr->board & CELL_MASK[ position ] == 0, "This cell is taken.");
-    _games.modify(game_itr, get_self(), [&](auto& this_game) {
+    
+    check( (primary_itr->board & CELL_MASK[ position ] ) == 0, "This cell is taken.");
+    _games.modify(primary_itr, get_self(), [&](auto& this_game) {
       this_game.board |= SET_CELL_HOST[ position ]; // cell is taken by host
       this_game.winner = get_winner(this_game.board);
       this_game.turns++;
       if(this_game.turns >= 8) this_game.winner = 3; // it is a tie game
     });
   }
-  else
-  {
-    auto idx = _games.get_index<name("bychallenger")>();
-    auto game_itr = idx.find(player.value);
+
+ if( (primary_itr == _games.end()) && (index_itr != idx.end()) ){ // is a challenger 
+    check(index_itr->winner == 0, "Game over: Winner=1: Host; 2: Challenger; 3: Draw");
+    uint8_t _turns = index_itr->turns; 
+    check( _turns % 2 == 0, "This is the Host's turn!" );
   
-    if( game_itr != idx.end()){ // player is the challenger
-      check(game_itr->winner == 0, "Game over: Winner=1: Host; 2: Challenger; 3: Draw");
-      uint8_t _turns = game_itr->turns; 
-      check( _turns % 2 == 0, "This is the Host's turn!" );
-      check( game_itr->board & CELL_MASK[ position ] == 0, "This cell is taken.");
-      idx.modify(game_itr, get_self(), [&](auto& this_game){  // idx.modify()????!!!
-        this_game.board |= SET_CELL_CHALLENGER[ position ]; // cell is taken by challenger
-        this_game.winner = get_winner(this_game.board);
-        this_game.turns++;
-        if(this_game.turns >= 8) this_game.winner = 3; // it is a draw
-      });
-    }
-    else
-    { // not found either in host's nor in challenger's list
-      check(false, "You are not in the game yet....!");
-    }
+    check( ( index_itr->board & CELL_MASK[ position ] ) == 0, "This cell is taken.");
+    idx.modify(index_itr, get_self(), [&](auto& this_game){  // idx.modify()????!!!
+      this_game.board |= SET_CELL_CHALLENGER[ position ]; // cell is taken by challenger
+      this_game.winner = get_winner(this_game.board);
+      this_game.turns++;
+      if(this_game.turns >= 8) this_game.winner = 3; // it is a draw
+    });
   }
   
 }
 
-EOSIO_DISPATCH(tictac3, (create)(close)(move))
+EOSIO_DISPATCH(tictac3, (create)(close)(move)(restart))
